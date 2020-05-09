ef7a1f4c651a397d46dd9a217b93cb5125bf2881
Renan Luquete <renanluquete@gmail.com]>
2020-05-09 15:27:02 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# IBPM-Desktop
---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+$(function() {
+    $('.multiple-items').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        arrows: false
+      });            
+});
